57352f1f895ff5a5c57020df6a31eaa6b6cd7314
Oleksandr Riabenkyi <o.riabenkyi@gmail.com>
2026-03-18 18:02:06 +0100

    fix makefile

--- ex03/src/main.cpp
+++ ex03/src/main.cpp
@@ -5,19 +5,47 @@ bool bsp( Point const a, Point const b, Point const c, Point const point );
 
 int main( void )
 {
-    Point a;
-    Point b(10, 0);
-    Point c(0, 10);
+    {
+        Point a;
+        Point b(-10, 0);
+        Point c(0, -10);
 
-    Point point(5, 5);
+        Point point(5, 5);
 
-    if (bsp(a, b, c, point)) {
-        std::cout << "The point is inside the triangle." << std::endl;
-    } else {
-        std::cout << "The point is outside the triangle." << std::endl;
+        if (bsp(a, b, c, point)) {
+            std::cout << "The point is inside the triangle." << std::endl;
+        } else {
+            std::cout << "The point is outside the triangle." << std::endl;
+        }
     }
-
     
+    {
+        Point a;
+        Point b(10, 0);
+        Point c(0, 10);
 
+        Point point(1, 1);
+
+        if (bsp(a, b, c, point)) {
+            std::cout << "The point is inside the triangle." << std::endl;
+        } else {
+            std::cout << "The point is outside the triangle." << std::endl;
+        }
+    }
+    {
+        Point a;
+        Point b(10, 0);
+        Point c(0, 10);
+
+        Point point(-5, -5);
+
+        if (bsp(a, b, c, point)) {
+            std::cout << "The point is inside the triangle." << std::endl;
+        } else {
+            std::cout << "The point is outside the triangle." << std::endl;
+        }
+    }    
+    
+    
     return 0;
 }
