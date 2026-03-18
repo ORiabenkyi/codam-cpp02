ac25fb7cd00fb0515fcc3f58b18f7fd6c32aeaa9
Oleksandr Riabenkyi <o.riabenkyi@gmail.com>
2026-03-18 18:18:22 +0100

    extend ex03

--- ex03/src/bsp.cpp
+++ ex03/src/bsp.cpp
@@ -10,8 +10,19 @@ bool bsp( Point const a, Point const b, Point const c, Point const point ) {
     Fixed areaBC = area(c - b, point - b);
     Fixed areaAB = area(b - a, point- a);
 
-    return !(areaAB > 0 && areaAC > 0 && areaBC > 0) && (areaAB < 0 && areaAC < 0 && areaBC < 0);
+    return !((areaAB > 0 || areaAC > 0 || areaBC > 0) && (areaAB < 0 || areaAC < 0 || areaBC < 0));
     
     //if are points are collinear, they are not considered to be inside the triangle
-    // return !(areaAB > 0 && areaAC > 0 && areaBC > 0) && (areaAB < 0 && areaAC < 0 && areaBC < 0) && (areaAB != 0 && areaAC != 0 && areaBC != 0);
+    // return !((areaAB > 0 || areaAC > 0 || areaBC > 0) && (areaAB < 0 || areaAC < 0 || areaBC < 0)) && (areaAB != 0 && areaAC != 0 && areaBC != 0);
+}
+
+bool bsp2( Point const a, Point const b, Point const c, Point const point ) {
+    Fixed areaAC = area(a - c, point - c);
+    Fixed areaBC = area(c - b, point - b);
+    Fixed areaAB = area(b - a, point- a);
+
+    // return !((areaAB > 0 || areaAC > 0 || areaBC > 0) && (areaAB < 0 || areaAC < 0 || areaBC < 0));
+    
+    //if are points are collinear, they are not considered to be inside the triangle
+    return !((areaAB > 0 || areaAC > 0 || areaBC > 0) && (areaAB < 0 || areaAC < 0 || areaBC < 0)) && (areaAB != 0 && areaAC != 0 && areaBC != 0);
 }
--- ex03/src/main.cpp
+++ ex03/src/main.cpp
@@ -6,11 +6,11 @@ bool bsp( Point const a, Point const b, Point const c, Point const point );
 int main( void )
 {
     {
-        Point a;
-        Point b(-10, 0);
-        Point c(0, -10);
+        Point a(0, 0);
+        Point b(5, 0);
+        Point c(0, 5);
 
-        Point point(5, 5);
+        Point point(1, 1);
 
         if (bsp(a, b, c, point)) {
             std::cout << "The point is inside the triangle." << std::endl;
@@ -20,7 +20,7 @@ int main( void )
     }
     
     {
-        Point a;
+        Point a(0, 0);
         Point b(10, 0);
         Point c(0, 10);
 
